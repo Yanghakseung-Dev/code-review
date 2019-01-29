e76486b9a3082df8ec288906911cf7b636022a53
y2000hs <y2000hs@gmail.com>
2019-01-29 17:11:03 +0900

    코드리뷰 파일

--- projects/Todos.jsx
+++ projects/Todos.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Todos = (props) => {
   console.log(props);
   return (
-  <div className="Todos" style={{backgroundColor:'#333'}}>
-    <h1>{props.title}</h1>
-    <ul>
-      {
-        props.items.map((item, index) => {
-          return <li key={props.title + index}><input type="checkbox" /> {item}</li>
-        })
-      }
-    </ul>
-  </div>
-)
+    <div className="Todos" style={{backgroundColor:'#333'}}>
+      <h1>{props.title}</h1>
+      <ul>
+        {
+          props.items.map((item, index) => {
+            return <li key={props.title+index}><input type="checkbox" defaultChecked={ item.completed }/>{ item.name }</li>
+          })
+        }
+      </ul>
+    </div>
+  )
 };
 export default Todos;
