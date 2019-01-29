89c19a4983182a529f6e07ce36911507bed6f64f
y2000hs <y2000hs@gmail.com>
2019-01-29 16:09:44 +0900

    소스

---
+++ projects/Todos.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Todos = (props) => {
+  console.log(props);
+  return (
+  <div className="Todos" style={{backgroundColor:'#333'}}>
+    <h1>{props.title}</h1>
+    <ul>
+      {
+        props.items.map((item, index) => {
+          return <li key={props.title + index}><input type="checkbox" /> {item}</li>
+        })
+      }
+    </ul>
+  </div>
+)
+};
+export default Todos;
